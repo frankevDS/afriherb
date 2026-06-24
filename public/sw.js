c420a58a7e131e7fa6709ef6536a4e5dcf07d596
frankevDS <frankevgloballtd@gmail.com>
2026-06-24 23:08:54 +0000

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,28 @@
-const CACHE_NAME = 'afriherb-v1';
-const urlsToCache = ['/'];
+const CACHE_NAME = 'afriherb-v2';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/icon-192-maskable.png',
+  '/icons/icon-512-maskable.png',
+  '/icons/apple-touch-icon.png',
+  '/icons/favicon-32.png',
+  '/icons/favicon-16.png'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // Clean up old cache versions so updates take effect promptly
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
 });
 
